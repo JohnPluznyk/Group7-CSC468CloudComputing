bc697c019458500512f1dd76febb55066eb62d62
JohnPluznyk <115428211+JohnPluznyk@users.noreply.github.com>
2024-04-03 16:19:12 -0400

    Create init.sql

---
+++ node/init.sql
@@ -0,0 +1,9 @@
+USE FusionBank;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON FusionBank.users TO 'john'@'%';
