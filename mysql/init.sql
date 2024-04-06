f5ba74dd2175c780adb986ee35f7982b17804e06
JohnPluznyk <115428211+JohnPluznyk@users.noreply.github.com>
2024-04-06 12:51:18 -0400

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -6,4 +6,16 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS bankingInfo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
 GRANT ALL PRIVILEGES ON FusionBank.users TO 'john'@'%';
